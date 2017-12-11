072803544ed6de91e36e1e31559f75322b7b63bd
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-12-10 22:11:04 -0700

    Modified KnapsackNode
    * Added nodeNumber
    * Made profit and weight calculated properties

--- src/KnapsackNode.java
+++ src/KnapsackNode.java
@@ -2,14 +2,37 @@ import java.util.List;
 
 public class KnapsackNode {
     public List<Good> goodsContained;
-    public int boundProfit, realProfit, weight;
+    public int nodeNumber, boundProfit, level;
 
-    public KnapsackNode (List<Good> goodsContained, int boundProfit,
-        int realProfit,
-        int weight) {
+    public KnapsackNode (int nodeNumber, int boundProfit,
+        int level, List<Good> goodsContained) {
+        this.nodeNumber = nodeNumber;
         this.goodsContained = goodsContained;
         this.boundProfit = boundProfit;
-        this.realProfit = realProfit;
-        this.weight = weight;
+        this.level = level;
+    }
+
+    public int getBoundProfit () {
+        return this.boundProfit;
+    }
+
+    public int getRealProfit () {
+        return goodsContained.stream().mapToInt(Good::getProfit).sum();
+    }
+
+    public int getRealWeight () {
+        return goodsContained.stream().mapToInt(Good::getWeight).sum();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Node ").append(nodeNumber).append(": ");
+        sb.append("items: [").append(goodsContained.toString()).append("], ");
+        sb.append("level: ").append(level).append(", ");
+        sb.append("profit: ").append(this.getRealProfit()).append(", ");
+        sb.append("weight: ").append(this.getRealWeight()).append(", ");
+        sb.append("bound: ").append(this.getBoundProfit());
+        return sb.toString();
     }
 }
