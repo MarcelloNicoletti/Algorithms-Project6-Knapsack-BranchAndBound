d5ff2d06f4bf827ce2f84b0ee2aad0b6446b8a6e
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-12-10 17:59:03 -0700

    Added Good Class

---
+++ src/Good.java
@@ -0,0 +1,39 @@
+public class Good {
+    private final String name;
+    private final int profit;
+    private final int weight;
+
+    Good (String name, int profit, int weight) {
+        this.name = name;
+        this.profit = profit;
+        this.weight = weight;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public int getProfit () {
+        return profit;
+    }
+
+    public int getWeight () {
+        return weight;
+    }
+
+    public double getScore () {
+        return (double) profit / (double) weight;
+    }
+
+    public int compareTo (Good other) {
+        int a = this.getProfit() * other.getWeight();
+        int b = other.getProfit() * this.getWeight();
+        return b - a;
+    }
+
+    @Override
+    public String toString () {
+        return String.format("(%s, p=%d, w=%d, p/w=%.2f)", this.name,
+            this.profit, this.weight, this.getScore());
+    }
+}
