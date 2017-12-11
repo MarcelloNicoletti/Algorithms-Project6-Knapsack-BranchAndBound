508c943f0d6e89890dfdc58f722f9ba15e27e51c
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-12-10 23:32:31 -0700

    So many java.util packages used.

--- src/Pro6.java
+++ src/Pro6.java
@@ -1,9 +1,6 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Pro6 {
