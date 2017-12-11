2a85949dba70d88721824f21d97511b39911c480
CupricWolf <marcello.s.nicoletti@gmail.com>
2017-12-10 19:53:16 -0700

    Added file reading logic

--- src/Pro6.java
+++ src/Pro6.java
@@ -1,2 +1,30 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Pro6 {
+    public static void main (String[] args) throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream(args[0]);
+        Scanner file = new Scanner(fis);
+
+        int maxWeight = file.nextInt();
+        List<Good> goods = getGoods(file);
+    }
+
+    private static List<Good> getGoods (Scanner file) {
+        List<Good> goods;
+        int numItems = file.nextInt();
+
+        goods = new ArrayList<>();
+        for (int i = 0; i < numItems; i++) {
+            int profit = file.nextInt();
+            int weight = file.nextInt();
+            Good good = new Good(Integer.toString(i + 1), profit, weight);
+            System.out.println(good.toString());
+            goods.add(good);
+        }
+        return goods;
+    }
 }
